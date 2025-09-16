59133256bbec3625d3ddb16e700b7be88cc5aa37
Valentine-Dimukeje <valentinedimukeje@gmail.com>
2025-09-16 13:11:41 +0100

    fix: update API_BASE logic in config.js

--- src/components/utils/config.js
+++ src/components/utils/config.js
@@ -1,6 +1,9 @@
-// src/config.js
 const isLocalhost = window.location.hostname === "localhost";
 
-export const API_BASE = isLocalhost
-  ? "http://127.0.0.1:8000" // Django local dev server
-  : "https://api.heritageinvestmentgrup.com"; // Django on Railway
+// Priority: if you set REACT_APP_API_BASE in Render → use that
+// Otherwise, fallback to localhost or production API
+export const API_BASE =
+  process.env.REACT_APP_API_BASE ||
+  (isLocalhost
+    ? "http://127.0.0.1:8000" // Django local dev server
+    : "https://api.heritageinvestmentgrup.com"); // Django on Railway
